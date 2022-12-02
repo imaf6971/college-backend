dd3902cf0e9e9d6aee5a8db1af80f22a8a667326
Vladislav Mironov <imaf6971@gmail.com>
2022-12-02 19:43:35 +0300

    changed types to be closer to JPA

--- src/main/resources/db/migration/V1__CREATE_TABLES.sql
+++ src/main/resources/db/migration/V1__CREATE_TABLES.sql
@@ -1,50 +1,50 @@
 CREATE TABLE IF NOT EXISTS faculties (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     faculty_code VARCHAR(4) NOT NULL UNIQUE,
     title VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS student_groups (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    admission_year YEAR NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    admission_year SMALLINT NOT NULL,
     after_highschool BOOLEAN NOT NULL,
     num INT NOT NULL,
-    faculty_id INT NOT NULL,
+    faculty_id BIGINT NOT NULL,
     FOREIGN KEY (faculty_id)
         REFERENCES faculties (id)
 );
 
 CREATE TABLE IF NOT EXISTS students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     patronymic VARCHAR(255),
-    group_id INT NOT NULL,
+    group_id BIGINT NOT NULL,
     FOREIGN KEY (group_id)
         REFERENCES student_groups (id)
 );
 
 CREATE TABLE IF NOT EXISTS plan_module (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS event_plan (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    module_id INT NOT NULL,
-    month DATE NOT NULL,
+    module_id BIGINT NOT NULL,
+    month SMALLINT NOT NULL,
     FOREIGN KEY (module_id)
         REFERENCES plan_module (id)
 );
 CREATE TABLE IF NOT EXISTS events (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    plan_id INT NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    plan_id BIGINT NOT NULL,
     FOREIGN KEY (plan_id)
         REFERENCES event_plan (id)
 );
 CREATE TABLE IF NOT EXISTS event_participants (
-    student_id INT NOT NULL,
-    event_id INT NOT NULL,
+    student_id BIGINT NOT NULL,
+    event_id BIGINT NOT NULL,
     PRIMARY KEY (student_id , event_id),
     FOREIGN KEY (student_id)
         REFERENCES students (id),
@@ -53,13 +53,13 @@ CREATE TABLE IF NOT EXISTS event_participants (
 );
 
 CREATE TABLE IF NOT EXISTS courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS courses_participants (
-    student_id INT NOT NULL,
-    course_id INT NOT NULL,
+    student_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
     PRIMARY KEY (student_id , course_id),
     FOREIGN KEY (student_id)
         REFERENCES students (id),
@@ -68,13 +68,13 @@ CREATE TABLE IF NOT EXISTS courses_participants (
 );
 
 CREATE TABLE IF NOT EXISTS council_position (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS students_council (
-    student_id INT NOT NULL,
-    position_id INT NOT NULL,
+    student_id BIGINT NOT NULL,
+    position_id BIGINT NOT NULL,
     PRIMARY KEY (student_id , position_id),
     FOREIGN KEY (student_id)
         REFERENCES students (id),
